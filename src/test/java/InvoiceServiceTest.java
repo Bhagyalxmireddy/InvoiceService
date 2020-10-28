044c8625767b7047dec97e146aaf964655f3af59
Bhagi <puppyreddy1997@gmail.com>
2020-10-28 09:50:44 +0530

    [Bhagyalaxmi]Added:Added code and Testcase to calculate MinimumFare  with distance & Time

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -1,5 +1,4 @@
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
@@ -11,5 +10,13 @@ public class InvoiceServiceTest {
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5,fare,0.0);
     }
 }
